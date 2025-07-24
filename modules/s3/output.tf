03424ba5261a11264e602daaab1bc762f7a2844f
davo <davcho2014@gmail.com>
2025-07-24 12:35:24 +0100

    added s3 bucket to dev environment

--- modules/s3/output.tf
+++ modules/s3/output.tf
@@ -2,6 +2,6 @@ output "s3-bucket-name" {
   value = aws_s3_bucket.test_bucket.bucket
 }
 
-output "test-bucket-2" {
-  value = aws_s3_bucket.test_bucket-2.bucket
-}
+# output "test-bucket-2" {
+#   value = aws_s3_bucket.test_bucket-2.bucket
+# }
