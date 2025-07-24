e518d4d19f7baae9d0796cc9adfe02438022245a
davo <davcho2014@gmail.com>
2025-07-24 12:32:14 +0100

    added s3 bucket to dev environment

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -2,3 +2,13 @@ resource "aws_s3_bucket" "test_bucket" {
   bucket        = "${var.projectName}-${var.environment}-my-test-random-bucket-a-a-a"
   force_destroy = true
 }
+
+resource "aws_s3_bucket" "test_bucket-2" {
+  count         = var.environment == "dev" ? 1 : 0 # Create only in dev
+  bucket        = "${var.projectName}-${var.environment}-my-test-random-bucket-b-b-b"
+  force_destroy = true
+  tags = {
+    Name        = "${var.projectName}-${var.environment}-test-bucket"
+    Environment = var.environment
+  }
+}
--- modules/s3/output.tf
+++ modules/s3/output.tf
@@ -1,3 +1,7 @@
 output "s3-bucket-name" {
   value = aws_s3_bucket.test_bucket.bucket
 }
+
+output "test-bucket-2" {
+  value = aws_s3_bucket.test_bucket-2.bucket
+}
